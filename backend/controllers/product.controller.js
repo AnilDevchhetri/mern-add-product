110e5d7e742aeff00f5813c801909683a30b94c4
AnilDevchhetri <chhetrivision@gmail.com>
2025-09-05 12:09:03 +0900

    55

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -8,3 +8,47 @@ export const getProudct = async (req, res) => {
         res.status(500).json({ success: false, message: "Server error" })
     }
 }
+
+export const createProduct = async (req, res) => {
+    const product = req.body;
+
+    if (!product.name || !product.price || !product.image) {
+        return res.status(400).json({ success: false, message: "Please provide all fields" })
+    }
+
+
+    const newProduct = new Product(product);
+
+    try {
+        await newProduct.save();
+        res.status(201).json({ success: true, data: newProduct })
+    } catch (error) {
+        console.log("error in create product", error.message)
+        res.status(500).json({ success: false, message: "server error" })
+    }
+}
+
+export const updateProduct = async (req, res) => {
+    const { id } = req.params;
+    const product = req.body;
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({ success: false, message: "Invalid Product Id" })
+    }
+    try {
+        const updatedProudct = await Product.findByIdAndUpdate(id, product, { new: true });
+        return res.status(201).json({ success: true, data: updatedProudct })
+    } catch (error) {
+        return res.status(404).json({ success: false, message: "SErver error" })
+    }
+}
+
+export const deleteProduct = async (req, res) => {
+    const { id } = req.params;
+    try {
+        await Product.findByIdAndDelete(id);
+        res.status(200).json({ success: true, message: "Product Deleted" })
+    } catch (error) {
+        res.status(404).json({ success: false, message: "Product not found" })
+    }
+}
--- backend/server.js
+++ backend/server.js
@@ -7,13 +7,14 @@ import router from "./routes/product.route.js";
 
 dotenv.config()
 const app = express();
+const PORT = process.env.PORT || 5000;
 
 app.use(express.json());//allwo use json data in body it is middleware
 
 
-app.use("/api/products", router)
+app.use("/api", router)
 
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
     console.log("server startss at 5000")
 })
@@ -22,4 +23,8 @@ app.listen(5000, () => {
 // project name mongo: mern-add-product
 
 //https://www.youtube.com/watch?v=O3BUHwfHf84&ab_channel=freeCodeCamp.org
-//50
+//55
+
+//jsut for refrecne as it not a proudction website
+//MONGO_URI = mongodb+srv://chhetrivision_db_user:s3x8n3eKLFvAymuw@cluster0.idakmzl.mongodb.net/products?retryWrites=true&w=majority&appName=Cluster0
+//PORT = 5000
